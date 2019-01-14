6aaef68a689f5a6c1a06eac4ccad3976eb56a2cf
recca <flamerecca711@gmail.com>
2019-01-15 01:33:04 +0800

    renew

--- app/Services/FANNService.php
+++ app/Services/FANNService.php
@@ -1,19 +1,28 @@
 <?php
 namespace App\Services;
 
+use Illuminate\Support\Facades\Log;
+
 class FANNService
 {
-    public function train()
+    public function initial()
     {
-        $num_input = 3;
-        $num_output = 1;
-        $num_layers = 3;
-        $num_neurons_hidden = 10;
-        $desired_error = 0.00000000000000000000001;
-        $max_epochs = 500000;
+        $num_input = 12;
+        $num_output = 12;
+        $num_layers = 5;
+        $num_neurons_hidden = 100;
+        $desired_error = 0.01;
+        $max_epochs = 5000;
         $epochs_between_reports = 10;
 
-        $ann = fann_create_standard($num_layers, $num_input, $num_neurons_hidden, $num_output);
+        $ann = fann_create_standard(
+            $num_layers,
+            $num_input,
+            $num_neurons_hidden,
+            $num_neurons_hidden,
+            $num_neurons_hidden,
+            $num_output
+        );
 
         if ($ann) {
             fann_set_activation_function_hidden($ann, FANN_SIGMOID_SYMMETRIC);
@@ -27,19 +36,87 @@ class FANNService
         }
     }
 
+    public function train()
+    {
+
+        $train_file = (dirname(__FILE__) . "/xor_float.net");
+        if (!is_file($train_file))
+            die("The file xor_float_1.net has not been created! Please run simple_train.php to generate it");
+
+        $origin_ann = fann_create_from_file($train_file);
+
+        $num_input = 12;
+        $num_output = 12;
+        $num_layers = 5;
+        $num_neurons_hidden = 100;
+        $desired_error = 0;
+        $max_epochs = 500;
+        $epochs_between_reports = 10;
+
+        $ann = fann_create_standard(
+            $num_layers,
+            $num_input,
+            $num_neurons_hidden,
+            $num_neurons_hidden,
+            $num_neurons_hidden,
+            $num_output
+        );
+
+        if (!$ann)
+            die("ANN could not be created");
+
+
+        fann_set_activation_function_hidden($ann, FANN_SIGMOID_SYMMETRIC);
+        fann_set_activation_function_output($ann, FANN_SIGMOID_SYMMETRIC);
+
+        $filename = dirname(__FILE__) . '/xor.data';
+
+        $file_resource = fann_read_train_from_file($filename);
+
+        $origin_MSE = fann_test_data($origin_ann , $file_resource);
+
+        if (fann_train_on_file($ann, $filename, $max_epochs, $epochs_between_reports, $desired_error)) {
+            $new_MSE = fann_test_data($ann , $file_resource);
+            if($new_MSE < $origin_MSE){
+                echo "fann_save()\n$new_MSE:$origin_MSE\n";
+                fann_save($ann, dirname(__FILE__) . "/xor_float.net");
+                Log::info("fann_save()\n$new_MSE:$origin_MSE\n");
+            } else {
+                echo "fann fail to save\n$new_MSE:$origin_MSE\n";
+                Log::info("fann fail to save\n$new_MSE:$origin_MSE\n");
+            }
+        } else {
+            echo 'fann_train_on_file() fail';
+        }
+        fann_destroy($ann);
+    }
+
     public function test()
     {
         $train_file = (dirname(__FILE__) . "/xor_float.net");
         if (!is_file($train_file))
-            die("The file xor_float.net has not been created! Please run simple_train.php to generate it");
+            die("The file xor_float.net has not been created!");
 
         $ann = fann_create_from_file($train_file);
         if (!$ann)
             die("ANN could not be created");
 
-        $input = array(1, 0, 0);
+        $file_resource = fann_read_train_from_file((dirname(__FILE__) . '/xor.data'));
+        if (!is_resource($file_resource))
+            die("resource fail");
+
+        $input = [0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1];
         $calc_out = fann_run($ann, $input);
-        printf("xor test (%f,%f, %f) -> %f\n", $input[0], $input[1], $input[2], $calc_out[0]);
+        printf(
+            "test (%.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f) \n"
+             . "->   (%.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f)\n",
+            $input[0], $input[1], $input[2], $input[3], $input[4], $input[5],
+            $input[6], $input[7], $input[8], $input[9], $input[10], $input[11],
+            $calc_out[0], $calc_out[1], $calc_out[2], $calc_out[3], $calc_out[4], $calc_out[5],
+            $calc_out[6], $calc_out[7], $calc_out[8], $calc_out[9], $calc_out[10], $calc_out[11]
+        );
+
+        echo fann_test_data($ann , $file_resource) . "\n";
         fann_destroy($ann);
     }
 }
